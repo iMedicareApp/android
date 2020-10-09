b04ef8e9e317df596f3c2a40a60cbec2ddcc715e
Kolawole Akinde <akinde.kolawole@gmail.com>
2020-10-09 05:37:11 +0100

    First Commit

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.androidapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,10 +17,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextInputEditText password;
+    TextInputLayout passwordLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        password=findViewById(R.id.password);
+        passwordLayout=findViewById(R.id.passwordLayout);
+        
     }
 }
